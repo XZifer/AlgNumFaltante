c56c3444c30477d4658cf3f155e74f0cb45d7fa8
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-19 17:33:59 -0500

    Program consola listo

--- NumeroFaltanteAlgoritmo/Program.cs
+++ NumeroFaltanteAlgoritmo/Program.cs
@@ -1,2 +1,8 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("¿Cual es el numero faltante en la matriz?");
+
+Algoritmo AlgNumFaltante = new Algoritmo();
+int[] A = {2,3,1,5};
+int Faltante;
+Faltante = AlgNumFaltante.NumFaltante(A);
+Console.WriteLine(A);
+Console.WriteLine("Falta {0} en la matriz",Faltante);
