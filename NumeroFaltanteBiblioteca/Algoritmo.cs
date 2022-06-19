1df6b2ce6d0fdccfcd64ddb43d9e2715e4e5daa5
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-19 17:05:26 -0500

    Agregar archivos de proyecto.

---
+++ NumeroFaltanteBiblioteca/Algoritmo.cs
@@ -0,0 +1,7 @@
+﻿namespace NumeroFaltanteBiblioteca
+{
+    public class Algoritmo
+    {
+
+    }
+}
---
+++ NumeroFaltantePrueba/NumeroFaltanteUnitTest.cs
@@ -0,0 +1,11 @@
+namespace NumeroFaltantePrueba
+{
+    [TestClass]
+    public class NumeroFaltanteUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
